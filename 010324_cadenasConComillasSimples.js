4ba849f1f78826c0eed7ea9b4a22716d505e7dc7
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-24 23:10:16 -0300

    Create(JAVASCRIPT): creación de nuevos archivos js

---
+++ 010324_cadenasConComillasSimples.js
@@ -0,0 +1,18 @@
+// 01:03:24 "CADENAS DE CARACTERES CON COMILLAS SIMPLES"
+
+/*
+- (' ')
+ */
+
+var miMeta;
+
+miMeta = "Aprender a programar con 'freeCodeCamp'.";
+console.log(miMeta);
+
+
+miMeta = 'Aprender a programar con "freeCodeCamp".';
+console.log(miMeta);
+
+
+
+// node 010324_cadenasConComillasSimples.js
---
+++ 010555_secuenciasDeEscape.js
@@ -0,0 +1,37 @@
+// 01:05:55 "SECUENCIAS DE ESCAPE"
+
+/*
+Código | Resultado
+------------------
+\'       Comilla simple
+\"       Comilla doble
+\\       Barra invertida
+\n       Línea nueva
+\r       Retorno de carro
+\t       Tabulación
+\b       Retroceso
+\f       Salto de página
+...
+*/
+
+
+// Ejemplo 1 - Comillas dobles
+
+console.log("Estoy aprendiendo \"Javascript\".");
+
+
+// Ejemplo 2 - Barra invertida
+
+console.log("Estoy aprendiendo \\ Javascript");
+
+
+// Ejemplo 3 - Línea nueva
+
+console.log("Estoy aprendiendo: \n Javascript");
+
+
+
+
+// node 010555_secuenciasDeEscape.js
+
+
