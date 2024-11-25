005b019b1d92bc93044999920ed23b6bd79bdfc3
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-24 23:30:28 -0300

    Create(JAVASCRIPT): creación de nuevos archivos js

---
+++ 011014_concatenarCadenasDeCaracteres.js
@@ -0,0 +1,15 @@
+// 01:10:14 "CONCATENAR CADENAS DE CARACTERES"
+
+var nombreCompleto = "Alan " + "Turing";
+console.log(nombreCompleto);
+
+
+var nombreCompleto = "Alan" + " Turing";
+console.log(nombreCompleto);
+
+
+var nombreCompleto = "Alan" + " " + "Turing";
+console.log(nombreCompleto);
+
+
+// node 011014_concatenarCadenasDeCaracteres.js
---
+++ 011254_construirCadenasConVariables.js
@@ -0,0 +1,11 @@
+// 011254 "CONSTRUIR CADENAS CON VARIABLES"
+
+var verbo = "programar";
+
+var mensaje = "Estoy aprendiendo a " + verbo;
+
+console.log(mensaje);
+
+
+
+// node 011254_construirCadenasConVariables.js
