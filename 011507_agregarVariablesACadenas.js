1c59a03504207f6279cbf715febcb0c57014c71a
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-25 21:46:07 -0300

    Create(JAVASCRIPT): creación de nuevos archivos js

---
+++ 011507_agregarVariablesACadenas.js
@@ -0,0 +1,14 @@
+// 01:15:07 "AGREGAR VARIABLES A CADENAS DE CARACTERES"
+
+var mensajeCompleto = "Estoy aprendiendo a programar ";
+var parteFinal = "con freeCodeCamp."
+
+console.log(mensajeCompleto);
+
+console.log(mensajeCompleto + parteFinal);
+
+
+
+// node 011507_agregarVariablesACadenas.js
+
+
---
+++ 011757_longitudDeUnaCadena.js
@@ -0,0 +1,32 @@
+// 01:17:57 "LONGITUD DE UNA CADENA DE CARACTERES"
+
+/*
+- La longitud o tamaño de una cadena es simplemente cuantos caracteres contiene la cadena incluyendo espacios 
+y cualquier símbolo que pueda contener.
+
+*/
+
+
+var miCadena = "A";
+console.log(miCadena.length);
+
+var miCadena = "AB";
+console.log(miCadena.length);
+
+var miCadena = "A B";
+console.log(miCadena.length);
+
+var miCadena = "AB ";
+console.log(miCadena.length);
+
+var miCadena = "JavaScript";
+console.log(miCadena.length);
+
+var miCadena = "¡Estoy aprendiendo a programar!";
+console.log(miCadena.length);
+
+var miCadena = "Estoy aprendiendo a programar";
+console.log(miCadena.length);
+
+
+// node 011757_longitudDeUnaCadena.js
