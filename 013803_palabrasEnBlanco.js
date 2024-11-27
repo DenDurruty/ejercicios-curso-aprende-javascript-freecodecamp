54c97c7e6d604337153e3ddf4ab7745c813b89ef
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-27 14:48:20 -0300

    add (javascript): se agregó otro ejercicio al archivo

--- 013803_palabrasEnBlanco.js
+++ 013803_palabrasEnBlanco.js
@@ -1,10 +1,5 @@
 // 01:38:03 "PALABRAS EN BLANCO"
 
-var miSustantivo = "perro";
-var miAdjetivo = "negro";
-var miVerbo = "corrió";
-var miAdverbio = "rapidamente";
-
 /*
 - Concatena las cadenas para crear una cadena que muestre un mensaje. Puedes cambiar los 
 valores de las variables.
@@ -14,3 +9,39 @@ Por ej.
  La bicicleta pequeña voló a la tienda lentamente.
 
 */
+
+// Oración 1
+/*
+var miSustantivo = "perro";
+var miAdjetivo = "negro";
+var miVerbo = "corrió";
+var miAdverbio = "rápidamente";
+
+var palabrasEnBlanco = "El " + miSustantivo + miAdjetivo + miVerbo + miAdverbio + " a la tienda.";
+console.log(palabrasEnBlanco);
+
+
+var palabrasEnBlanco = "El" + " " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " " + miAdverbio + " " + "a la tienda.";
+console.log(palabrasEnBlanco);
+*/
+
+
+// Oración 2
+var miSustantivo = "bicicleta";
+var miAdjetivo = "pequeña";
+var miVerbo = "voló";
+var miAdverbio = "lentamente";
+
+var palabrasEnBlanco = "La " + miSustantivo + miAdjetivo + miVerbo + " a la tienda" + miAdverbio + ".";
+console.log(palabrasEnBlanco);
+
+
+var palabrasEnBlanco = "La" + " " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " " + "a la tienda" + " " + miAdverbio + ".";
+console.log(palabrasEnBlanco); 
+
+
+
+
+// node 013803_palabrasEnBlanco.js
+
+
