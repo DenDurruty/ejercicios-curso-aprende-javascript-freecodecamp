676235881b595fc65d60623cb0c7079361a4518e
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-26 21:57:54 -0300

    Create(JAVASCRIPT): creación de nuevos archivos js

---
+++ 013425_notacionDeCorchetesDeDerechaAIzq.js
@@ -0,0 +1,21 @@
+// 01:34:25 "NOTACIÓN DE CORCHETES: DE DERECHA A IZQUIERDA"
+
+/*
+- El penúltimo índice es longitud -2 porque comenzamos a contar desde 0.
+  miCadena.length es 10. El penúltimo índice es 8.
+
+*/
+var miCadena;
+miCadena = "Javascript";
+
+console.log(miCadena[miCadena.length -2]); // p
+
+
+var n;
+n = 4;
+
+console.log(miCadena[miCadena.length -n]); // r
+
+
+
+// node 013425_notacionDeCorchetesDeDerechaAIzq.js
---
+++ 013803_palabrasEnBlanco.js
@@ -0,0 +1,16 @@
+// 01:38:03 "PALABRAS EN BLANCO"
+
+var miSustantivo = "perro";
+var miAdjetivo = "negro";
+var miVerbo = "corrió";
+var miAdverbio = "rapidamente";
+
+/*
+- Concatena las cadenas para crear una cadena que muestre un mensaje. Puedes cambiar los 
+valores de las variables.
+
+Por ej. 
+ El perro negro corrió rapidamente a la tienda. 
+ La bicicleta pequeña voló a la tienda lentamente.
+
+*/
