a77b7aaa897006a62ef7a9163325b10050dac5ec
Den Durruty <dendurruty@users.noreply.github.com>
2024-11-26 21:38:02 -0300

    Create(JAVASCRIPT): creación de nuevos archivos js

---
+++ 012646_notacionDeCorchetesEnesimoCaracter.js
@@ -0,0 +1,32 @@
+// 01:26:46 "NOTACION DE CORCHETES: ENESIMO CARACTER"
+
+/*
+- 
+*/
+
+
+var miCadena = "JavaScript"; 
+            //  012345678
+
+console.log(miCadena[0]); // J
+console.log(miCadena[1]); // a
+console.log(miCadena[2]); // v
+console.log(miCadena[3]); // a
+console.log(miCadena[4]); // S
+console.log(miCadena[5]); // c
+console.log(miCadena[6]); // r
+console.log(miCadena[7]); // i
+console.log(miCadena[8]); // p
+console.log(miCadena[9]); // t         
+            
+
+
+// ¿Qué pasa si usamos un índice que está fuera de los límites permitidos?
+console.log(miCadena[10]); // Undefined
+
+
+
+
+
+
+// node 012646_notacionDeCorchetesEnesimoCaracter.js
---
+++ 013024_notacionDeCorchetesUltimoCaracter.js
@@ -0,0 +1,17 @@
+// 01:30:24 "NOTACIÓN DE CORCHETES: ÚLTIMO CARACTER"
+
+/*
+- El último índice siempre es la longitud -1 porque comenzamos a contar de desde cero.
+
+ */
+
+var miCadena = "JavaScript";
+//              0123456789
+// miCadena.length = 10, entonces 10 -1 = 9
+
+console.log(miCadena[miCadena.length-1]) // t
+
+
+
+// node 013024_notacionDeCorchetesUltimoCaracter.js
+
