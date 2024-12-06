6994c1ee7526469551dc5911f7efca4bf0ed9792
Den Durruty <dendurruty@users.noreply.github.com>
2024-12-06 00:42:09 -0300

    Update (javascript): se completaron ejercicios del archivo 090204 al 090855

--- 090204_definirUnaClase.js
+++ 090204_definirUnaClase.js
@@ -1,12 +1,40 @@
 // 09:02:04 "DEFINIR UNA CLASE"
 
 /*
-- 
+- Definir una clase es como crear un plano.
 
 */
 
 
 // Ejemplo 1
+class TransbordadorEspacial {
+    constructor(planeta) {
+        this.planeta = planeta;
+    }
+}
+
+
+var zeus = new TransbordadorEspacial("Jupiter");
+console.log(zeus.planeta);
+
+var apolo = new TransbordadorEspacial("Marte");
+console.log(apolo.planeta);
+
+
+// Ejemplo 2
+class Mascota {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+var miMascota = new Mascota("Nora", 5);
+var miMascota = new Mascota("Gino", 2);
+
+
+console.log(miMascota.nombre);
+console.log(miMascota.edad);
 
 
 
--- 090855_gettersSetters.js
+++ 090855_gettersSetters.js
@@ -7,7 +7,28 @@
 
 
 // Ejemplo 1
+class Libro {
+    constructor(autor) {
+        this._autor = autor;
+    }
 
+    get autor() {
+        console.log("Getter");
+        return this._autor;
+    }
+
+    set autor(nuevoAutor) {
+        console.log("Setter");
+        this._autor = nuevoAutor;
+    }
+}
+
+
+const libro = new Libro("anónimo");
+console.log(libro.autor);
+
+libro.autor = "Gino Smith";
+console.log(libro.autor);
 
 
 // node 090855_gettersSetters.js
